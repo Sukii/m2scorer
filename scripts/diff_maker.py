dab37dd6f80bcada990304774e5946a8ed53d8de
sukii <suki.venkat@gmail.com>
2022-09-24 22:51:35 +0530

    reading arrays from files

--- scripts/diff_maker.py
+++ scripts/diff_maker.py
@@ -1,18 +1,30 @@
+import sys
 import difflib
 
-# define original text
-original = ["The birds in this forest is singing beautifuly","Another sentence with error in punctiation"]
 
-# define modified text
-edited = ["The birds in this forest are singing beautifully","Another sentence, with an error in punctuation"]
+# read original sentences into array
+original_file = open(sys.argv[1], "r")
+original = original_file.readlines()
+original_file.close()
+
+# read modified sentences into array
+modified_file = open(sys.argv[2], "r")
+edited = modified_file.readlines()
+modified_file.close()
+
+# original sentences
+#original = ["The birds in this forest is singing beautifuly","Another sentence with error in punctiation"]
+
+# corrected sentences
+#edited = ["The birds in this forest are singing beautifully","Another sentence, with an error in punctuation"]
 
 # initiate the Differ object
 d = difflib.Differ()
 
 for i in range(0,len(original)):
-    print(original[i])
-    # calculate the difference between the two texts
-    diff = list(d.compare(original[i].split(), edited[i].split()))
+    print(original[i].rstrip())
+    # calculate the difference between the two sentences
+    diff = list(d.compare(original[i].rstrip().split(), edited[i].rstrip().split()))
     type = "unknown"
     i = 0
     for word in diff:
